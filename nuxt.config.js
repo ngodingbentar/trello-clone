0afa721a5a20be0c7330f64ed9d713bca528414b
ngodingbentar <dewaqintoro@gmail.com>
2021-05-28 18:16:01 +0700

    added env

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,12 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING,
+          appId: process.env.APP_ID
         },
         services: {
           auth: {
