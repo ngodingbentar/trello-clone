6ba1dfe7aaaaa1072b9caccc048df903b2cae4ed
ngodingbentar <dewaqintoro@gmail.com>
2021-05-28 23:42:39 +0700

    migrate to composition api in index

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
           </v-row>
           <v-icon @click="dialog = false">mdi-close</v-icon>
         </v-row>
-        <v-form ref="form" v-model="valid">
+        <!-- <v-form ref="form" v-model="valid">
           <div class="d-flex flex-column">
             <v-text-field
               label="Board title"
@@ -72,7 +72,7 @@
               >Submit</v-btn
             >
           </div>
-        </v-form>
+        </v-form> -->
       </v-container>
     </v-dialog>
     <div class="d-flex flex-row align-center justify-space-between">
@@ -83,20 +83,20 @@
       <p v-if="boards.length === 0">You have no boards yet.</p>
       <v-card
         :style="board.image.downloadURL != '' ? `background:url(${board.image.downloadURL});`: board.color ? `background-color:${board.color}` : ''"
-        @click="$router.push('/boards/' + board.id)"
         class="jello-board-tile"
         v-for="board in boards"
         v-bind:key="board.id"
+        @click="$router.push('/boards/' + board.id)"
       >
-        <v-card-title :style="board.image.downloadURL != '' ? 'color:#fff':''">
+        <v-card-title>
           {{ board.title }}
         </v-card-title>
-        <v-card-subtitle :style="board.image.downloadURL != '' ? 'color:#fff':''">
+        <v-card-subtitle>
           created {{ board.dateCreated | formatDate }}
         </v-card-subtitle>
       </v-card>
     </div>
-    <v-snackbar
+    <!-- <v-snackbar
       :timeout="3000"
       v-model="snackbar"
       absolute
@@ -104,42 +104,37 @@
       color="primary"
     >
       {{ snackbarText }}
-    </v-snackbar>
+    </v-snackbar> -->
   </v-container>
 </template>
 
 <script>
+import { computed, defineComponent, reactive, ref, useContext } from '@nuxtjs/composition-api'
 import { v4 as uuidv4 } from 'uuid'
-export default {
-  async asyncData() {
-    //lets get our board data before page load, and then after that await changes
-    let boardsRef = $nuxt.$fire.firestore
-      .collection('users')
-      .doc($nuxt.$fire.auth.currentUser.uid)
-      .collection('boards')
-    let boardData = []
-    await boardsRef
-      .get()
-      .then(function (querySnapshot) {
-        if (querySnapshot.docs.length > 0) {
-          try {
-            for (const doc of querySnapshot.docs) {
-              let data = doc.data()
-              data.id = doc.id
-              boardData.push(data)
-            }
-          } catch (err) {}
-        }
-      })
-      .catch(function (error) {})
-    return { boards: boardData }
-  },
-  data() {
-    return {
-      enableColor: false,
-      dialog: false,
-      valid: false,
-      board: {
+
+export default defineComponent({
+  setup() {
+    const { route } = useContext()
+    const pageId = route.value.params.id
+
+    const dialog = ref(false)
+    const dialogCard = ref(false)
+    const dialogEditCard = ref(false)
+    const drawer = ref(false)
+    const currentCard=ref({})
+    const cardDraggedId = ref('')
+    const cardDraggedListId = ref('')
+    const boards = ref([])
+    const currentImageId=ref('')
+    const listId=ref('')
+    const list=ref({
+      title: 'dew'
+    })
+    const card=ref({
+      title: ''
+    })
+
+    const board= ref({
         title: '',
         color: '',
         image: {
@@ -148,138 +143,63 @@ export default {
           downloadURL: '',
           uuid: '',
         },
-      },
-      snackbar: false,
-      snackbarText: 'No error message',
-      currentImageId: '',
-      fileToUpload: {},
-    }
-  },
-  created() {
-    //lets watch our boards just to give it that realtime feel when we add or remove boards.
-    let that = this
-    $nuxt.$fire.firestore
-      .collection(`users/${$nuxt.$fire.auth.currentUser.uid}/boards/`)
-      .onSnapshot(function (querySnapshot) {
-        if (querySnapshot.docs.length > 0) {
-          that.boards = []
-          try {
-            for (const doc of querySnapshot.docs) {
-              let data = doc.data()
-              data.id = doc.id
-              that.boards.push(data)
-            }
-          } catch (err) {}
-        }
       })
-  },
-  methods: {
-    addBoard() {
-      //lets create a temp id we can use to save our doc and our storage files
-      this.currentImageId = uuidv4()
-      this.dialog = true
-    },
-    createBoard() {
-      let that = this
-      if (this.$refs.form.validate()) {
-        //Let's give our board a created date.
-        this.board.dateCreated = Date.now()
-        this.$fire.firestore
-          .collection('users')
-          .doc(this.$fire.auth.currentUser.uid)
-          .collection('boards')
-          .doc(this.currentImageId)
-          .set(this.board)
-          .then(function (docRef) {
-            that.dialog = false
-            that.$refs.form.reset()
-            that.snackbarText = 'Successfully created your board'
-            that.snackbar = true
-          })
-          .catch(function (error) {})
-      }
-    },
+
+    const boardData = ref([])
+    const data = ref({})
+
+    asyncData()
     
-    chooseImage() {
-      this.$refs['boardBackground'].click()
-    },
-    onFileClicked($event) {
-      $event.target.value = ''
-    },
-    onFileSelected(event) {
-      try {
-        /// Process Selected Files
-        let file = event.target.files[0]
-        this.fileToUpload = {
-          file,
-          originalName: file.name,
-          loading: false,
-          progress: 0,
-          success: false,
-          error: false,
-          previewPath: '',
-          uuid: uuidv4(),
-        }
-        this.uploadFiles()
-      } catch (error) {}
-    },
-    uploadFiles() {
-      const itemFilename =
-        this.fileToUpload.uuid + '-' + this.fileToUpload.file.name
-      const itemName =
-        'images' +
-        '/' +
-        this.$fire.auth.currentUser.uid +
-        '/' +
-        'boards' +
-        '/' +
-        this.currentImageId +
-        '/' +
-        itemFilename
 
-      const itemRef = this.$fire.storage.ref().child(itemName)
-      const itemMeta = {
-        customMetadata: {
-          owner: this.$fire.auth.currentUser.uid,
-        },
-      }
+    const tempId = ref('')
 
-      // Action upload
-      const task = itemRef.put(this.fileToUpload.file, itemMeta)
+    return {
+      dialog,
+      boards,
+      board,
+      currentImageId,
+      addBoard
+    }
 
-      // Watch for upload status Changes
-      return task.on(
-        'state_changed',
-        // Handle progress
-        (progress) => {
-          // Set upload progress on item
-          this.fileToUpload.progress =
-            (progress.bytesTransferred / progress.totalBytes) * 100
-        },
-        // Handle unsuccessful uploads
-        (error) => {
-          this.fileToUpload.failed = true
-          this.fileToUpload.error = error
-          return false
-        },
-        // Handle successful uploads on complete
-        async () => {
-          const url = await task.snapshot.ref
-            .getDownloadURL()
-            .catch((e) => false)
-          // Store form media
-          this.board.image = {
-            name: itemFilename,
-            originalName: this.fileToUpload.file.name,
-            downloadURL: url,
-            uuid: this.fileToUpload.uuid,
+    async function asyncData() {
+      // console.log('params', params)
+      const boardRef = $nuxt.$fire.firestore
+        .collection('users')
+        .doc($nuxt.$fire.auth.currentUser.uid)
+        .collection('boards')
+
+      await boardRef
+        .get()
+        .then(function (querySnapshot) {
+          if (querySnapshot.docs.length > 0) {
+            try {
+              for (const doc of querySnapshot.docs) {
+                data.value = doc.data()
+                data.value.id = doc.id
+                boardData.value.push(data.value)
+              }
+            } catch (err) {
+              console.log(err)
+            }
           }
-        }
-      )
-    },
+        })
+        .catch(function (error) {})
+        console.log('boardData.value', boardData.value)
+        boards.value = boardData.value
+        tempId.value = boardData.value.id
+        // createdFunc()
+    }
+
+    function addBoard() {
+      //lets create a temp id we can use to save our doc and our storage files
+      currentImageId.value = uuidv4()
+      dialog.value = true
+    }
+
   },
-}
+})
 </script>
+
 <style lang="scss">
 .v-dialog {
   border-radius: 15px;
